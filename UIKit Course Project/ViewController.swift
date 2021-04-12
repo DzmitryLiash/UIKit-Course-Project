185000bfcb5f196a96accd48537559d76b076257
Dzmitry Liashchou <liashchou.d@gmail.com>
2021-04-12 15:49:10 +0200

    1. Segmented Control 2. Slider (without styles and actions)

--- UIKit Course Project/ViewController.swift
+++ UIKit Course Project/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Setup main label
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        // Setup segment control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
     }
 
-
+    @IBAction func changeSegment() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .blue
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .white
+        case 2:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .yellow
+        default:
+            break
+        }
+    }
+    
 }
 
